5452bb6f6c27556350683b15424f209f596eea13
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2025-03-22 16:16:01 +0700

    read CLI arguments with process.argv

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ message('Andin');
 // untuk mendapatkan informasi penggunaan memory ketika proses berjalan
 const memoUsage = process.memoryUsage();
 console.log(memoUsage);
+
+// jika kita menjalankan node index.js "harry" "potter" 
+// array process.argv akan bernilai:
+// Elemen pertama : Alamat (path) lengkap dari lokasi node yang menjalankan prosesnya. 
+// Element kedua : Alamat (path) berkas JavaScript yang dieksekusi (app.js)
+// Element ketiga : “harry”
+// Element keempat : “potter”
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+console.log(`Hello ${firstName} ${lastName}`);
